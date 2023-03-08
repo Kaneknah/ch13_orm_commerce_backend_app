67040b713840d45733c47656311faec48d42151d
Kaneknah <jamesnbaxley@protonmail.com>
2023-03-07 19:35:57 -0700

    created product find and findAll functions

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,18 +5,59 @@ const { Product, Category, Tag, ProductTag } = require("../../models");
 
 // get all products
 router.get("/", (req, res) => {
+	  try {
+    const productData = await Product.findAll({
+		include: [
+			{model: Category},
+			{model: Tag},
+		],
+	  });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 	// find all products
 	// be sure to include its associated Category and Tag data
-});
+
 
 // get one product
 router.get("/:id", (req, res) => {
+		try {
+    const productData = await Product.findByPk(req.params.id, {
+      
+      include: [
+		{ model:Category},
+		{model: Tag},
+	  ],
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No products found with this id!' });
+      return;
+    }
+
+    res.status(200).json(travellerData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
+      // JOIN with locations, using the Trip through table
+      
+    });
 	// find a single product by its `id`
 	// be sure to include its associated Category and Tag data
-});
+
 
 // create new product
 router.post("/", (req, res) => {
+	try {
+    const productData = await Product.create(req.body);
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 	/* req.body should look like this...
     {
       product_name: "Basketball",
